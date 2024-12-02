a7a1c4c540c8c8c003d9af1298d97e0f97c0d678
MinhKhanh614 <167750602+MinhKhanh614@users.noreply.github.com>
2024-12-02 17:58:50 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,15 @@
-/**
- * Giá trị Analog của cảm biến
- */
-/**
- * Giá trị (%) của cảm biến
- */
-let dataPercent = 0
-let dataAnalog = 0
-// Bật cổng Serial
-serial.setBaudRate(BaudRate.BaudRate115200)
+// Khi nhấn nút A, Relay được bật
+input.onButtonPressed(Button.A, function () {
+    lcd.displayText("ON ", 8, 1)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+// Khi nhấn nút B, Relay bị tắt
+input.onButtonPressed(Button.B, function () {
+    lcd.displayText("OFF", 8, 1)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
 // Xóa toàn bộ nội dung trên LCD (nếu có)
 lcd.clearScreen()
-// Cho hiển thị tiêu đề trước
-lcd.displayText("Light Detector", 1, 1)
-lcd.displayText("[LDR] " + lcd.displaySymbol(lcd.Symbols.sym02), 1, 2)
-basic.forever(function () {
-    // Đọc giá trị Analog của cảm biến và đổi ra thang (%)
-    dataAnalog = pins.analogReadPin(AnalogPin.P0)
-    dataPercent = Math.round(Math.map(dataAnalog, 0, 1023, 100, 0))
-    // Cho hiển thị giá trị (%) của cảm biến trên LCD
-    lcd.displayText("" + dataPercent + "%  ", 9, 2)
-    // Gửi giá trị (%) của cảm biến lên Serial
-    serial.writeLine("" + (dataPercent))
-    // Dừng 0.5s
-    basic.pause(500)
-})
+// Cho hiển thị tiêu đề trên LCD và tắt Relay
+lcd.displayText("Relay: OFF", 1, 1)
+pins.digitalWritePin(DigitalPin.P0, 1)
